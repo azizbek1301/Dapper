91063647dd956660a437f647d0d9c8a31322d312
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-10-30 12:31:19 +0500

    Add project files.

---
+++ DapperLesson/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using DapperLesson.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace DapperLesson.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private string connectionString = WebApplication.CreateBuilder().Configuration.GetConnectionString("DefaultConnection");
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                string query = "Select * from Test";
+                var users = connection.Query<User>(query);
+
+                return Ok(users);
+            }
+        }
+        [HttpPost]
+        public IActionResult GetlUser()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                string query = "Select * from Test";
+                var users = connection.Query<User>(query);
+
+                return Ok(users);
+            }
+        }
+    }
+}
---
+++ DapperLesson/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace DapperLesson.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public int age { get; set; }
+    }
+}
